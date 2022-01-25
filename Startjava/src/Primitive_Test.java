ac887bd9a41f0665bf5c8257e1ae5249d83f8e87
김 선 후 <tjsgnrla97@gmail.com>
2022-01-25 10:05:15 +0900

    DailyJava
    자바 학습

---
+++ Startjava/src/Primitive_Test.java
@@ -0,0 +1,19 @@
+
+public class Primitive_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 10;		
+		System.out.println(num1);
+		//8진수
+		int num2 = 012;
+		System.out.println(num2);
+		//16진수
+		int num3 = 0xA;
+		System.out.println(num3);
+		//float
+		float a = 3.5f;
+		System.out.println(a);
+	}
+
+}
